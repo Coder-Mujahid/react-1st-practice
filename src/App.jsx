ad95b64a2a6471c40daf2edf1237262674ec2b50
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-15 01:59:49 +0600

    1st nav bar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import img from '../img/logo-black.png'
 
 function App() {
   
@@ -18,15 +19,19 @@ export default App
 function Details(){
   return(
     <nav>
-      <ul className="flex justify-center">
-        <li className="mr-5">
-        home
+      <ul className="w-11/12 mx-auto items-center flex justify-between">
+        <li>
+          <img className='w-16 p-3' src={img} alt="" />
         </li>
-        <li className="mr-5">
-        about
-        </li>
-        <li className="mr-5">
-        blogspot
+
+        <li>
+          <ul className='flex justify-center gap-5 text-base uppercase font-semibold '>
+            <li><a className='border-amber-300 hover:border-b-2 px-2 transition-all duration-200' href="#">home</a></li>
+            <li><a className='border-amber-300 hover:border-b-2 px-2 transition-all duration-200' href="#">about</a></li>
+            <li><a className='border-amber-300 hover:border-b-2 px-2 transition-all duration-200' href="#">service</a></li>
+            <li><a className='border-amber-300 hover:border-b-2 px-2 transition-all duration-200' href="#">blogs</a></li>
+            <li><a className='border-amber-300 hover:border-b-2 px-2 transition-all duration-200' href="#">utpal er nanir heda</a></li>
+          </ul>
         </li>
       </ul>
     </nav>
