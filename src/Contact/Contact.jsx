90c3cbfbb4908bd90368b7b7c0117c7929b82b5e
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-16 00:37:36 +0600

    responsive the contact sectoin

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,38 +1,39 @@
 
 const Contact = () => {
   return (
-    <section className="text-gray-600 body-font relative w-11/12 mx-auto bg-gradient-to-r from-slate-50 to-slate-300">
+    <>
+    
+        <section className="text-gray-600 body-font relative w-11/12 mx-auto bg-gradient-to-r from-slate-50 to-slate-300">
+        <h2 className='text-5xl text-blue-900 font-semibold pt-10 uppercase text-center'>Contact me</h2>
+
       <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
             className="absolute inset-0"
-            frameBorder="0"
             title="map"
-            marginHeight="0"
-            marginWidth="0"
-            scrolling="no"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13274.598351015944!2d90.42999658827215!3d23.76643435712345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78c71227c15%3A0x9f0818437919415d!2sAftab%20Nagar%2C%20Dhaka!5e1!3m2!1sen!2sbd!4v1700072490905!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d273.33899220314714!2d90.43846892760122!3d23.764721069199826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78c71227c15%3A0x9f0818437919415d!2sAftab%20Nagar%2C%20Dhaka!5e1!3m2!1sen!2sbd!4v1700073366935!5m2!1sen!2sbd"
           ></iframe>
-          <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+          <div className="bg-slate-200 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-              <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+              <h2 className="title-font capitalize font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+              <p className="mt-1">Nursery Road , Aftab Nagar ,joharul Haque City , Dhaka</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
               <a href="#" className="text-indigo-500 leading-relaxed">
-                example@email.com
+              mujahidislam400301@gmail.com
               </a>
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">+8801737705511</p>
             </div>
           </div>
         </div>
-        <div className="lg:w-1/3 md:w-1/2 p-3 rounded-md bg-gradient-to-r from-slate-300 to-slate-50 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-          <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+        {/* massage section */}
+        <div className="lg:w-1/3 md:w-1/2 p-4 rounded-md bg-gradient-to-r from-slate-300 to-slate-50 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact me </h2>
+          <p className="leading-relaxed mb-5 text-gray-600">If you like to know more about my work or process feel free to get in touch.</p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">
               Name
@@ -72,7 +73,7 @@ const Contact = () => {
         </div>
       </div>
     </section>
-  );
-};
-
+    </>
+  )
+}
 export default Contact;
